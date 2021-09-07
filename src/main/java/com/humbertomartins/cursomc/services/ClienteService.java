ecbb06a28a642c949a2aba9f49cc0b87e80965e7
Humberto9s1 <humbertosant.mart@gmail.com>
2021-09-07 00:32:01 -0300

    Validacao customizada de CPF ou CNPJ na insercao de cliente

--- src/main/java/com/humbertomartins/cursomc/services/ClienteService.java
+++ src/main/java/com/humbertomartins/cursomc/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.humbertomartins.cursomc.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -34,6 +36,8 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
+	
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
 		obj = repo.save(obj);
